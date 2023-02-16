18c4e8de7fccc36777cff14f9abeb59c33ecc113
Scott Phillips <polygonguru@gmail.com>
2023-02-16 21:02:18 +0000

    build: Use new github output format

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -12,5 +12,5 @@ $json = Invoke-WebRequest "https://api.github.com/repos/$Repo/releases/latest" `
 
 $version = if ([string]::IsNullOrEmpty($ManualVersion)) { $json.tag_name.Trim("v") } else { $ManualVersion.Trim("v") }
 
-Write-Output "::set-output name=releaseVersion::$($json.tag_name)"
-Write-Output "::set-output name=packageVersion::$version"
+echo "releaseVersion=$($json.tag_name)" >> $env:GITHUB_OUTPUT
+echo "packageVersion=$version" >> $env:GITHUB_OUTPUT
