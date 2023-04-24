ed858e401d2822c8274d3484b6979c041d90eb69
Scott Phillips <polygonguru@gmail.com>
2023-04-24 13:00:46 -0400

    Update Set-Version.ps1

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -6,15 +6,14 @@ param
     $GithubToken,
     $ManualVersion
 )
-$json = ""
 try {
     $json = Invoke-WebRequest "https://api.github.com/repos/$Repo/releases/latest" `
         -Headers @{ "Authorization" = "Token $GithubToken" } | ConvertFrom-Json
+    $version = if ([string]::IsNullOrEmpty($ManualVersion)) { $json.tag_name.Trim("v") } else { $ManualVersion.Trim("v") }
 } catch { 
     # Do nothing if the release is manually overridden
+    $version = $ManualVersion.Trim("v")
 }
-
-$version = if ([string]::IsNullOrEmpty($ManualVersion)) { $json.tag_name.Trim("v") } else { $ManualVersion.Trim("v") }
 
 echo "releaseVersion=$($json.tag_name)" >> $env:GITHUB_OUTPUT
 echo "packageVersion=$version" >> $env:GITHUB_OUTPUT
